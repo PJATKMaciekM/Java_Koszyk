0dc38edf65b057be7da58f3776e640d98993121b
PJATKMaciekM <146550964+PJATKMaciekM@users.noreply.github.com>
2024-05-25 00:31:00 +0200

    Update Koszyk.java
    rescue

--- src/main/Koszyk.java
+++ src/main/Koszyk.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Koszyk {
     public ArrayList<Product> koszyk_zak;
@@ -16,11 +18,14 @@ public class Koszyk {
     public Product getProduct(int idx){
         return koszyk_zak.get(idx);
     }
-    public Product getProducts(int idx){
-        for(int i=0; i<idx; i++){
-            return koszyk_zak.get(i);
+    public List<Product> getProducts(int idx){
+        if(idx >= 0 && idx < koszyk_zak.size()){
+            return koszyk_zak.subList(0, idx);
         }
         return null;
+    }
+    public int getCount() {
+        return count;
     }
     public double getSum() {
         double sum = 0;
@@ -29,6 +34,41 @@ public class Koszyk {
         }
         return sum;
     }
+    public List<Product> sort(Comparator<Product> comparator){
+        koszyk_zak.sort(comparator);
+        return koszyk_zak;
+    }
+
 
 }
+Comparator<Product> asc = new Comparator<Product>() {
+    public int compare(Product p1, Product p2) {
+        return Double.compare(p1.getPrice(), p2.getPrice());
+    }
+};
+Comparator<Product> dsc = new Comparator<Product>() {
+    public int compare(Product p1, Product p2) {
+        return Double.compare(p2.getPrice(), p1.getPrice());
+    }
+};
 
+abstract class sortUp implements Comparator<Product> {
+    public int compare(Product p1, Product p2) {
+        return (int)(p1.getPrice() - p2.getPrice());
+    }
+}
+abstract class sortDown implements Comparator<Product> {
+    public int compare(Product p1, Product p2) {
+        return (int)(p2.getPrice() - p1.getPrice());
+    }
+}
+class sortAphUp implements Comparator<Product> {
+    public int compare(Product p1, Product p2) {
+        return p1.getName().compareTo(p2.getName());
+    }
+}
+abstract class sortAphDown implements Comparator<Product> {
+    public int compare(Product p1, Product p2) {
+        return p2.getName().compareTo(p1.getName());
+    }
+}
